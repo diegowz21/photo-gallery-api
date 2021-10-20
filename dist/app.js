1d195499c344c0ecb595ea86042d5c2fe8bf15c2
Diego Cerna <diegowz21@gmail.com>
2021-10-19 19:36:30 -0500

    make it better

--- dist/app.js
+++ dist/app.js
@@ -7,12 +7,14 @@ const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const index_1 = __importDefault(require("./routes/index"));
 const path_1 = __importDefault(require("path"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 //settings 
 app.set('port', process.env.PORT || 4000);
 //middlewares
 app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 //routes
 app.use('/api', index_1.default);
 //this folder for this app will be used to store public files
